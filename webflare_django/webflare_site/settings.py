7ea6cafcc103fd4d014ec47b264300a4123ab424
WebFlare UK <webflareuk@gmail.com>
2025-10-28 13:40:11 +0000

    Pushing github changes

--- webflare_django/webflare_site/settings.py
+++ webflare_django/webflare_site/settings.py
@@ -62,8 +62,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
